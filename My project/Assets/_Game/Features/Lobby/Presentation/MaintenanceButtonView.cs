522d0ea2f46b120fb7d31c13702b43e5db177ff5
jshagold <jshagold@gmail.com>
2025-12-09 22:29:27 +0900

    feat: 정비 씬으로 가는 버튼 View 구현

---
+++ My project/Assets/_Game/Features/Lobby/Presentation/MaintenanceButtonView.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.Localization.Settings;
+
+public class MaintenanceButtonView : MonoBehaviour
+{
+    [SerializeField] private Button btn;
+    [SerializeField] private Image iconImg;
+    [SerializeField] private TextMeshProUGUI textMesh;
+
+    [Header("Resources")]
+    [SerializeField] private Sprite normalSprite;
+
+    private const string TextTableName = "UI TABLE";
+
+    public void SetOnClickAction(UnityAction action)
+    {
+        btn.onClick.RemoveAllListeners();
+        btn.onClick.AddListener(action);
+    }
+
+    // --- 화면 갱신용 함수들 ---
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowNormalMode()
+    {
+        gameObject.SetActive(true);
+        iconImg.sprite = normalSprite;
+        string text = LocalizationUtils.GetString(tableName: TextTableName, key: "scene_main_maintenance_button");
+        textMesh.text = text;
+    }
+ 
+}
