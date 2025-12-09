aa2fae0be9a80da9fba73222bf05867703b5c808
jshagold <jshagold@gmail.com>
2025-12-09 22:28:55 +0900

    feat: 텍스트 로컬라이징 용 패키지 추가 - Localization

---
+++ My project/Assets/_Game/Core/Utils/LocalizationUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Localization.Settings;
+
+public static class LocalizationUtils
+{
+    public static string GetString(string tableName, string key)
+    {
+        // 아직 로딩이 안 됐거나 에러가 날 경우를 대비해 예외처리나 빈 값 처리를 할 수도 있습니다.
+        var op = LocalizationSettings.StringDatabase.GetLocalizedString(tableName, key);
+
+        // 만약 키를 못 찾으면 키 값 자체를 반환하거나 에러 로그를 띄우도록 커스텀 가능
+        return string.IsNullOrEmpty(op) ? key : op;
+    }
+}
