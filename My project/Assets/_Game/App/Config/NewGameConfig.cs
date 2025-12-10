470485abb709176d341990e6b0813ba664724106
jshagold <jshagold@gmail.com>
2025-12-10 23:58:03 +0900

    feat: 초기 게임 세팅값 설정

---
+++ My project/Assets/_Game/App/Config/NewGameConfig.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NewGameConfig", menuName = "SamSara/App/NewGameConfig")]
+public class NewGameConfig : ScriptableObject
+{
+    [Header("Inventory Settings")]
+    public int InitialMoney = 0; // 기본 소지금
+
+    [Header("Daily State Settings")]
+    public int StartDay = 1; // 시작 날짜
+    public int DefaultActionSlots = 3; // 기본 행동력 슬롯 개수
+
+    [Header("Character Settings")]
+    public List<string> StartingCharacterIds = new List<string> { "character_main" };
+
+}
