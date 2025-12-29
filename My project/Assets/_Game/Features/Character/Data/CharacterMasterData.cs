d8027efeb07723234bec283b55dd829f6ccc56e4
hakee <jshagold@gmail.com>
2025-12-29 11:09:11 +0900

    feat: 캐릭터 기능들을(Summary, Detail, Data) Character폴더로 통합, 캐릭터 불변 데이터(기획 데이터) 추가

---
+++ My project/Assets/_Game/Features/Character/Data/CharacterMasterData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterMasterData", menuName = "Samsara/Character Master Data")]
+public class CharacterMasterData : ScriptableObject
+{
+    [Header("Identity")]
+    public int id;
+    public string characterName;
+    [TextArea] public string desc;
+
+    [Header("Visual")]
+    public Sprite portrait;
+
+    [Header("Base Stats")]
+    public float baseHp;
+    public int baseStrength;
+    public int baseToughness;
+    public int baseAgility;
+
+    [Header("Limit Stats")]
+    public float limitHp;
+    public int limitStrength;
+    public int limitToughness;
+    public int limitAgility;
+
+    [Header("Skills")]
+    public SkillMasterData mainSkill;
+    public SkillMasterData subSkill1;
+    public SkillMasterData subSkill2;
+
+}
