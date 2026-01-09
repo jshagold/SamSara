375eb5cfcbdc26613e4fbcc4699eb3d8971a6563
hakee <jshagold@gmail.com>
2026-01-09 13:45:29 +0900

    feat: CharacterDetailBootstrapper 뼈대 구성

---
+++ My project/Assets/_Game/Features/Character/Detail/Presentation/CharacterDetailBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CharacterDetailBootstrapper: MonoBehaviour
+{
+    private readonly string _logClass = $"[{nameof(CharacterDetailBootstrapper)}]";
+
+    public void Initialize()
+    {
+
+        Debug.Log($"{_logClass} Initialize");
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
