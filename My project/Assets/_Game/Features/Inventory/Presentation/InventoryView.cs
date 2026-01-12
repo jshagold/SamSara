4d0f45266956846d9f15cd5f25137fbbc26d063e
hakee <jshagold@gmail.com>
2026-01-12 16:57:34 +0900

    feat: ItemIconView, InventoryView 구현

---
+++ My project/Assets/_Game/Features/Inventory/Presentation/InventoryView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryView : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private ScrollRect _scrollRect;
+    [SerializeField] private Transform _contentRoot;
+
+    [Header("Prefab")]
+    [SerializeField] private ItemIconView _itemIconPrefab;
+
+    private List<ItemIconView> _spawnedItemList = new List<ItemIconView>();
+
+    public void ClearList()
+    {
+        foreach(var item in _spawnedItemList)
+        {
+            if(item != null) Destroy(item.gameObject);
+        }
+        _spawnedItemList.Clear();
+
+        _scrollRect.horizontalNormalizedPosition = 0f;
+    }
+
+    public void AddItem(ItemInfo info, Sprite iconImage, Action onClickItemAction)
+    {
+        var newItemView = Instantiate(_itemIconPrefab, _contentRoot);
+        
+        newItemView.SetData(info: info, iconImage: iconImage);
+        newItemView.SetOnClickIcon(onClickItemAction);
+        _spawnedItemList.Add(newItemView);
+    }
+}
--- My project/Assets/_Game/Features/Item/Presentation/ItemIconView.cs
+++ My project/Assets/_Game/Features/Item/Presentation/ItemIconView.cs
@@ -1,13 +1,21 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ItemIconView : MonoBehaviour
 {
+    [Header("Frame")]
+    [SerializeField] private GameObject _contentsRoot;
     [SerializeField] private Image _iconFrame;
-    [SerializeField] private Image _iconImg;
 
+    [Header("Icon/CountText")]
+    [SerializeField] private Image _iconImage;
     [SerializeField] private TextMeshProUGUI _countText;
+
+    [Header("Interaction")]
+    [SerializeField] private Button _iconButton;
 
     private void Reset()
     {
@@ -17,35 +25,46 @@ public class ItemIconView : MonoBehaviour
         {
             string objName = image.gameObject.name.ToLower();
 
-            if (_iconFrame == null && objName.Contains("frame")) _iconFrame = image;
-            if (_iconImg == null && objName.Contains("img")) _iconImg = image;
+            if(_iconFrame == null && objName.Contains("frame")) _iconFrame = image;
+            if(_iconImage == null && objName.Contains("image")) _iconImage = image;
         }
 
-        if (_countText == null) _countText = GetComponent<TextMeshProUGUI>();
+        if(_countText == null) _countText = GetComponentInChildren<TextMeshProUGUI>(true);
+        if(_iconButton == null) _iconButton = GetComponentInChildren<Button>(true);
     }
 
-    private void OnDestroy()
+    public void SetData(ItemInfo info, Sprite iconImage)
     {
-
-    }
-
-    public void SetData(ItemInfo info, ItemMasterData masterData)
-    {
-        if(masterData != null)
+        if(iconImage == null)
         {
-            _iconImg.sprite = masterData.Icon;
-            _iconImg.enabled = true;
+            _iconFrame.enabled = true;
+            _contentsRoot.SetActive(false);
+            return;
+        }
+
+        _contentsRoot.SetActive(true);
+        _iconFrame.enabled = true;
+
+        _iconImage.sprite = iconImage;
+        // TODO ItemType에 따른 Item Frame 나중에 설정해야함.
+
+        bool showCount = info.Count > 0;
+        _countText.gameObject.SetActive(showCount);
+        if(showCount)  _countText.text = info.Count.ToString();
+    }
+
+    public void SetOnClickIcon(Action action)
+    {
+        _iconButton.onClick.RemoveAllListeners();
+
+        if(action != null)
+        {
+            _iconButton.onClick.AddListener(() => action.Invoke());
+            _iconButton.interactable = true;
         }
         else
         {
-            _iconImg.sprite = null;
-            _iconImg.enabled = false;
-        }
-
-        if(info != null)
-        {
-            _countText.text = info.Count.ToString();
-            // TODO 나중에 Item Frame 추가
+            _iconButton.interactable = false;
         }
     }
 }
