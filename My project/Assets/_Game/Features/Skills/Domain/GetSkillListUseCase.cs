81fc5cc01bab7c295b05b41065d00060875ab0bd
hakee <jshagold@gmail.com>
2026-01-14 16:26:48 +0900

    feat: SkillUseCase, SkillPresenter 구현

---
+++ My project/Assets/_Game/Features/Skills/Domain/GetSkillListUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class GetSkillListUseCase
+{
+    private readonly ICharacterRepository _characterRepo;
+
+    public GetSkillListUseCase(ICharacterRepository characterRepo)
+    {
+        _characterRepo = characterRepo;
+    }
+
+    public List<SkillInfo> Execute()
+    {
+        CharacterInfo characterInfo = _characterRepo.GetCharacterInfo();
+        EvolutionNodeInfo currentNodeInfo = characterInfo.CurrentEvolutionNode;
+
+        return currentNodeInfo.SkillList;
+    }
+
+    public event Action OnSkillListChanged
+    {
+        add => _characterRepo.OnCharacterUpdated += value;
+        remove => _characterRepo.OnCharacterUpdated -= value;
+    }
+}
---
+++ My project/Assets/_Game/Features/Skills/Presentation/SkillListPresenter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillListPresenter : IDisposable
+{
+    // Views
+    private readonly SkillListView _skillListView;
+
+    // UseCases
+    private readonly GetSkillListUseCase _getSkillListUseCase;
+
+    // SkillMasterData
+    private readonly ISkillMasterRepository _skillMasterRepo;
+
+    public SkillListPresenter(
+        SkillListView skillListView,
+        GetSkillListUseCase getSkillListUseCase,
+        ISkillMasterRepository skillMasterRepo) 
+    {
+        _skillListView = skillListView;
+        _getSkillListUseCase = getSkillListUseCase;
+        _skillMasterRepo = skillMasterRepo;
+    }
+
+    public void Initialize()
+    {
+        _getSkillListUseCase.OnSkillListChanged += Refresh;
+
+        Refresh();
+    }
+
+    private void Refresh()
+    {
+        _skillListView.ClearList();
+
+        List<SkillInfo> skillList = _getSkillListUseCase.Execute();
+        foreach (SkillInfo skillInfo in skillList)
+        {
+            SkillMasterData skillMasterData = _skillMasterRepo.GetData(skillId: skillInfo.Id);
+            Sprite icon = skillMasterData.Icon;
+
+            _skillListView.AddSkill(skillInfo:skillInfo, iconImage: icon, onClickSkillIcon: () => OnClickSkill(skillInfo));
+        }
+    }
+
+    private void OnClickSkill(SkillInfo skillInfo)
+    {
+        Debug.Log($"스킬 클릭: {skillInfo.Id} {skillInfo.Name}");
+    }
+
+    public void Dispose()
+    {
+        _getSkillListUseCase.OnSkillListChanged -= Refresh;
+    }
+}
