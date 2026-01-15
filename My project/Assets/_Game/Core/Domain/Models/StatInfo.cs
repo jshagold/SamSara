5578f5d4ce4f4cf138f3485186acb63a765c74d6
hakee <jshagold@gmail.com>
2026-01-15 14:30:00 +0900

    feat: StatInfo 수정, CharacterDetail에서 사용할 Display용 데이터 class 구현

--- My project/Assets/_Game/Core/Domain/Models/StatInfo.cs
+++ My project/Assets/_Game/Core/Domain/Models/StatInfo.cs
@@ -4,16 +4,8 @@ using Newtonsoft.Json;
 [Serializable]
 public struct StatInfo
 {
-    [JsonProperty("label")]
+    [JsonIgnore]
     public string Label;
     [JsonProperty("value")]
     public float Value;
-    [JsonProperty("limit")]
-    public float Limit;
-
-    [JsonIgnore]
-    public float Ratio => (Limit > 0) ? Value / Limit : 0f;
-
-    [JsonIgnore]
-    public bool IsMax => Value >= Limit;
 }
---
+++ My project/Assets/_Game/Features/Character/Detail/Domain/StatDisplayInfo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public struct StatDisplayInfo
+{
+    public string Label;
+    public float CurrentValue;
+    public float StartValue;
+    public float MaxValue;
+
+    public float Ratio
+    {
+        get
+        {
+            if(MaxValue <= StartValue) return 0f;
+
+            float current = Mathf.Clamp(CurrentValue, StartValue, MaxValue);
+            return (current - StartValue) / (MaxValue - StartValue);
+        }
+    }
+}
