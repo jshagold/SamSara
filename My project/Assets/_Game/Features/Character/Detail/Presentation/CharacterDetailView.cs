ca0b5501e76bf13ae1d87cd2f5a04892136a69b4
hakee <jshagold@gmail.com>
2026-01-16 16:57:11 +0900

    feat: CharacterDetailView 구현

--- My project/Assets/_Game/Features/Character/Detail/Presentation/CharacterDetailView.cs
+++ My project/Assets/_Game/Features/Character/Detail/Presentation/CharacterDetailView.cs
@@ -1,6 +1,90 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterDetailView : MonoBehaviour
 {
-    
+    [Header("Top Area")]
+    [SerializeField] private Button _backButton;
+    [SerializeField] private Button _optionButton;
+    [SerializeField] private Image _portraitImage;
+    [SerializeField] private Button _evolutionSceneButton;
+
+    [Header("Contents Area")]
+    [SerializeField] private Image _titleBg;
+    [SerializeField] private TextMeshProUGUI _titleText;
+    [SerializeField] private Image _characterNameBg;
+    [SerializeField] private TextMeshProUGUI _characterNameText;
+    [SerializeField] private ScrollRect _scrollRect;
+
+
+    private void Reset()
+    {
+        Button[] buttons = GetComponentsInChildren<Button>(true);
+        foreach(Button button in buttons)
+        {
+            string objName = button.gameObject.name.ToLower();
+            if(_backButton == null && objName.Contains("back")) _backButton = button;
+            if(_optionButton == null && objName.Contains("option")) _optionButton = button;
+            if(_evolutionSceneButton == null && objName.Contains("evolution")) _evolutionSceneButton = button;
+        }
+
+        Image[] images = GetComponentsInChildren<Image>(true);
+        foreach(Image image in images)
+        {
+            string objName = image.gameObject.name.ToLower();
+            if (_portraitImage == null && objName.Contains("portrait")) _portraitImage = image;
+            if (_titleBg == null && objName.Contains("title")) _titleBg = image;
+            if (_characterNameBg == null && objName.Contains("character")) _characterNameBg = image;
+        }
+
+        TextMeshProUGUI[] texts = GetComponentsInChildren<TextMeshProUGUI>(true);
+        foreach(TextMeshProUGUI text in texts)
+        {
+            string objName = text.gameObject.name.ToLower();
+            if(_titleText == null && objName.Contains("title")) _titleText = text;
+            if(_characterNameText == null && objName.Contains("character")) _characterNameText = text;
+        }
+
+        if(_scrollRect == null) _scrollRect = GetComponentInChildren<ScrollRect>(true);
+    }
+
+    public void OnClickBackButton(Action action)
+    {
+        _backButton.onClick.RemoveAllListeners();
+        _backButton.onClick.AddListener(() => action.Invoke());
+    }
+
+    public void OnClickOptionButton(Action action)
+    {
+        _optionButton.onClick.RemoveAllListeners();
+        _optionButton.onClick.AddListener(() => action.Invoke());
+    }
+
+    public void OnClickEvoSceneButton(Action action)
+    {
+        _evolutionSceneButton.onClick.RemoveAllListeners();
+        _evolutionSceneButton.onClick.AddListener(() => action.Invoke());
+    }
+
+    public void SetTitleText(string text)
+    {
+        _titleText.text = text;
+    }
+
+    public void SetCharacterName(string name)
+    {
+        _characterNameText.text = name;
+    }
+
+    public void SetCharacterImage(Sprite sprite)
+    {
+        _portraitImage.sprite = sprite;
+    }
+
+    public void OnPreRender()
+    {
+        _scrollRect.verticalNormalizedPosition = 1f;
+    }
 }
