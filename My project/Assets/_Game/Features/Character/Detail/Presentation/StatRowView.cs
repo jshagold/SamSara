f25c77ee2d6c60e8c3a46e0354a8dda8cded761f
hakee <jshagold@gmail.com>
2026-01-16 16:57:00 +0900

    feat: stat관련 View 구현

--- My project/Assets/_Game/Features/Character/Detail/Presentation/StatRowView.cs
+++ My project/Assets/_Game/Features/Character/Detail/Presentation/StatRowView.cs
@@ -1,22 +1,38 @@
 ﻿using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StatRowView : MonoBehaviour
 {
     [Header("UI Components")]
-    [SerializeField] private TextMeshProUGUI _nameText; // "체력"
+    [SerializeField] private TextMeshProUGUI _labelText; // "체력"
     [SerializeField] private TextMeshProUGUI _currentText;  // 현재 스탯수치
     [SerializeField] private TextMeshProUGUI _maxText;  // 최대 스탯수치
-    [SerializeField] private Slider _gaugeSlider;   // 게이지 slider
+    [SerializeField] private Slider _statSlider;   // 스탯바 slider
+
+    [Header("Settings")]
+    [SerializeField] private string _valueFormat = "F0";
 
     private void Reset()
     {
-        
+        TextMeshProUGUI[] allTexts = GetComponentsInChildren<TextMeshProUGUI>(true);
+        foreach(var text in allTexts)
+        {
+            var objName = text.gameObject.name.ToLower();
+            if(_labelText == null && objName.Contains("label")) _labelText = text;
+            if(_currentText == null && objName.Contains("current")) _currentText = text;
+            if(_maxText == null && objName.Contains("max")) _maxText = text;
+        }
+
+        if(_statSlider == null) _statSlider = GetComponentInChildren<Slider>(true);
     }
 
-    public void SetData()
+    public void SetData(StatDisplayInfo statInfo)
     {
-
+        _labelText.text = statInfo.Label;
+        _currentText.text = statInfo.CurrentValue.ToString(_valueFormat);
+        _maxText.text = statInfo.MaxValue.ToString(_valueFormat);
+        _statSlider.value = statInfo.Ratio;
     }
 }
