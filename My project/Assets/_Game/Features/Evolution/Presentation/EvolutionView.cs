455368e10dca9a5395134b485dc13067509a90e4
hakee <jshagold@gmail.com>
2026-01-28 16:59:15 +0900

    feat: 진화단계 화면 View 구현중

---
+++ My project/Assets/_Game/Features/Evolution/Presentation/EvolutionView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EvolutionView : MonoBehaviour
+{
+    [Header("Top Area")]
+    [SerializeField] private Button _backButton;
+
+
+    private void Reset()
+    {
+        
+    }
+}
